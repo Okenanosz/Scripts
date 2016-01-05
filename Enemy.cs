ed48892400b1cf86a14f3a6cb6b64dca67eef5e8
Okenanosz <okenanosz181@gmail.com>
2016-01-05 17:54:55 +0100

    jjj
    jjj

---
+++ Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Enemy : NetworkBehaviour
+{
+    public int MaxHealth;
+    [SyncVar]
+    public int CurrentHealth;
+    
+
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        if (!isServer)
+            return;
+        CurrentHealth -= damage;
+    }
+
+	
+}
---
+++ MouseInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MouseInput : NetworkBehaviour
+{
+    void RaycastTest()
+    {
+        if (!isLocalPlayer)
+            return;
+        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(MousePos, Vector2.zero);
+        Debug.DrawRay(MousePos, Vector2.down);
+        if (hit.collider != null)
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                CmdDoDamage(hit.collider);
+           
+    }
+    [Command]
+    void CmdDoDamage(Collider2D c)
+    {
+            Enemy enemy = c.GetComponent<Enemy>();
+            enemy.TakeDamage(10);
+    }
+    void Update()
+    {
+        RaycastTest();
+    }
+}
